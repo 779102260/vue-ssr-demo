f4da4bb12e55773a3fcc8f5d5e12d2f7876fc7ee
cc <cc@ccMac-Pro.local>
2023-11-04 20:57:36 +0800

    resolve会在ready只前执行

--- src/entry.client.js
+++ src/entry.client.js
@@ -9,34 +9,34 @@ if (window.__INITIAL_STATE__) {
     pinia.state.value = window.__INITIAL_STATE__
 }
 
+router.beforeResolve(async (to, from, next) => {
+    // 预取数据
+    const matched = router.getMatchedComponents(to)
+    const prevMatched = router.getMatchedComponents(from)
+    
+    // 一般情况getMatchedComponents获取的单个组件
+    // 当存在嵌套路由时，匹配到多个组件
+    // 多个组件是与之前重复的不再预取数据
+    let diffed = false
+    const activated = matched.filter((c, i) => {
+        return diffed || (diffed = (prevMatched[i] !== c))
+    })
+
+    if (!activated.length) {
+        return next()
+    }
+
+    // 预取数据
+    await Promise.all(activated.map(c => {
+        console.log('[CSR] asyncData', c)
+        return c.asyncData && c.asyncData({pinia, route: to})
+    }))
+
+    next()
+})
 // 提前解析路由配置中的异步组件，才能正确地调用组件中可能存在的路由钩子
 router.onReady(() => {
     console.log('[CSR] router onReady')
-    router.beforeResolve(async (to, from, next) => {
-        // 预取数据
-        const matched = router.getMatchedComponents(to)
-        const prevMatched = router.getMatchedComponents(from)
-        
-        // 一般情况getMatchedComponents获取的单个组件
-        // 当存在嵌套路由时，匹配到多个组件
-        // 多个组件是与之前重复的不再预取数据
-        let diffed = false
-        const activated = matched.filter((c, i) => {
-            return diffed || (diffed = (prevMatched[i] !== c))
-        })
-
-        if (!activated.length) {
-            return next()
-        }
-
-        // 预取数据
-        await Promise.all(activated.map(c => {
-            console.log('[CSR] asyncData', c)
-            return c.asyncData && c.asyncData({pinia, route: to})
-        }))
-
-        next()
-    })
 
     console.log('[CSR] mount app')
     app.$mount('#app')
